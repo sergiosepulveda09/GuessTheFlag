f0249b429c23c1d1cb13542a3b4df9d18dc3689e
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-06-10 11:29:01 -0700

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sergio Sepulveda on 2021-06-10.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
